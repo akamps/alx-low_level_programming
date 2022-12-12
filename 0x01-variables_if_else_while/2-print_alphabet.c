0cba422de3c10f16f992355576af43993733b4cb
Nicholas Akampurira <37149477+akamps@users.noreply.github.com>
2022-12-12 19:25:57 +0300

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,7 @@
 *Return: return 0
 */
 int main(void)
-{	
-	int l;
-	
-	for (l = 'a';	l <= 'z';	l++)
-	{putchar(l);	
-	}
-	putchar('\n');	
-	return (0);	
-}
+{	int l;	for (l = 'a';	l <= 'z';	l++)
+	{
+	putchar(l);	}
+	putchar('\n');	return (0);	}
